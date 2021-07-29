0bfa56e7535542105a4559f06a4bfdaeeac894da
Maciej Satkiewicz <maciej.satkiewicz@mim-solutions.pl>
2021-07-29 14:44:38 +0000

    DictDataset - default transform is None

--- src/robbytorch/datasets/dict_dataset.py
+++ src/robbytorch/datasets/dict_dataset.py
@@ -1,23 +1,20 @@
 import torch
-from torchvision import transforms
 from torchvision.datasets.folder import default_loader
 import pathlib
-from typing import Mapping, Optional, Callable, Union, Any
+from typing import Mapping, Optional, Callable, Any
 
 from ..utils import SizedIterable, PathLike, TensorLike
 
 
-
 class DictDataset(torch.utils.data.Dataset):
     """Base helper class for creating robust Datasets
-    
+
     Args:
         root: Root directory containing your data
         metadata: Any SizedIterable storing metadata for your data, could be a pandas.DataFrame etc.
         transform: If given, takes a single ndarray datapoint and returns a transformed torch tensor.
-            Default: `torchvision.transforms.ToTensor`.
     """
-    
+
     def __init__(self, root: PathLike, metadata: SizedIterable, transform: Optional[Callable[[TensorLike], torch.Tensor]] = None):
         self.root = pathlib.Path(root).expanduser()
         self.metadata = metadata
@@ -29,12 +26,12 @@ class DictDataset(torch.utils.data.Dataset):
 
     def load_data(self, idx: int) -> TensorLike:
         """Should return a dataitem for a given integer key.
-        
-        You don't need to apply self.transform in this function (it will be applied later in self.__getitem__). 
+
+        You don't need to apply self.transform in this function (it will be applied later in self.__getitem__).
         Hence the returned dataitem need not be a tensor.
         """
         raise NotImplementedError()
-    
+
     def load_target_dict(self, idx: int) -> Mapping[str, Any]:
         """Should return a dict of Any data. It will be merged with {"data": dataitem} and returned by self.__getitem__.
 
@@ -43,21 +40,17 @@ class DictDataset(torch.utils.data.Dataset):
         """
         raise NotImplementedError()
 
-    def __getitem__(self, idx: int) -> Mapping[str, torch.Tensor]:
-        """Returns a {"data": data, **self.load_target_dict(idx)} dataitem for a given integer key. 
-            
-        Every value has to be a torch.Tensor.
+    def __getitem__(self, idx: int) -> Mapping[str, Any]:
+        """Returns a {"data": data, **self.load_target_dict(idx)} dataitem for a given integer key.
 
-        Later torch.utils.data.DataLoader collates this dicts into a dict o batched tensors.
+        Later torch.utils.data.DataLoader collates this dicts into a dict o batched items.
         """
         data = self.load_data(idx)
-        
+
         if self.transform:
             data = self.transform(data)
-        else:
-            data = transforms.ToTensor()(data)
-        
-        return {"data": data, **self.load_target_dict(idx)}        
-        
+
+        return {"data": data, **self.load_target_dict(idx)}
+
     def __len__(self) -> int:
         return len(self.metadata)
